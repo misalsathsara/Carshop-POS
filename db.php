d7fb91def886e5891102b314797e063a43001037
PLsandun <sadunkingmax@gmail.com>
2024-08-05 19:10:27 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,22 +1,19 @@
 <?php
-// $server_name = "localhost:8889";
-// $username = "root";
-// $password = "root";
-// $db_name = "carshop_db";
-
-$server_name = "localhost:3308";
+$server_name = "localhost:8889";
 $username = "root";
-$password = "";
+$password = "root";
 $db_name = "carshop_db";
+
+// $server_name = "localhost:3308";
+// $username = "root";
+// $password = "";
+// $db_name = "carshop_db";
 
 $conn = new mysqli($server_name, $username, $password, $db_name);
 
 if($conn->connect_error){
     die('Error: ' . $conn->connect_error);
 
-}else{
-    //echo 'sucsess!';
 }
-
 
 ?>
